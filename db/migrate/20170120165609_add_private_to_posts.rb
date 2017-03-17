2e413155f4c8a9b004de158441a05537230e5c62
Minh Nguyen <nguyen_ba_minh@yahoo.com>
2017-03-17 08:39:32 -0700

    change default for private to false

--- db/migrate/20170120165609_add_private_to_posts.rb
+++ db/migrate/20170120165609_add_private_to_posts.rb
@@ -1,5 +1,5 @@
 class AddPrivateToPosts < ActiveRecord::Migration[5.0]
   def change
-    add_column :posts, :private, :boolean, :default => 0
+    add_column :posts, :private, :boolean, :default => false
   end
 end
